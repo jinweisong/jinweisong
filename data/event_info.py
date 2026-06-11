baeea68e22dfe408bbe2570e3070a4d81854d5d3
qiu <yxy302351331@qq.com>
2026-06-11 21:53:36 +0800

    update event information

--- data/event_info.py
+++ data/event_info.py
@@ -1,3 +1,3 @@
-EVENT_NAME = "班级活动报名与统计网页"
-EVENT_DESCRIPTION = "用 Flask 做一个可以填写表单、提交报名、查看统计结果的动态网页。"
-EVENT_NOTICE = "本项目由 5 人小组通过 GitHub Issue、Fork、Pull Request 和 Review 协作完成。"
+EVENT_NAME = "第15周班级网页报名系统"
+EVENT_DESCRIPTION = "这是我们小组通过 Flask 和 GitHub 协作完成的动态网页。"
+EVENT_NOTICE = "所有修改都通过 Fork + Pull Request 进入主仓库。"
